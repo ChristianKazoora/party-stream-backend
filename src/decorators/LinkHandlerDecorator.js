f060fe3984e6093f9585736ea2f116c1135ed384
ChristianKazoora <christianmkazooora@yahoo.com>
2025-03-15 21:19:34 -0300

    feat: add pCloud link handling in LinkHandlerDecorator

--- src/decorators/LinkHandlerDecorator.js
+++ src/decorators/LinkHandlerDecorator.js
@@ -1,7 +1,7 @@
 const dropbox = require("./link_decorators/dropbox");
 const youtube = require("./link_decorators/youtube");
 const localFile = require("./link_decorators/localFile");
-
+const pCloud = require("./link_decorators/pCloud");
 class LinkHandlerDecorator {
   constructor(videoHandler) {
     this.videoHandler = videoHandler;
@@ -18,6 +18,8 @@ class LinkHandlerDecorator {
       linkData = youtube(videoPath);
     } else if (videoPath.includes("file://")) {
       linkData = localFile(videoPath);
+    } else if (videoPath.includes("pcloud.com")) {
+      linkData = pCloud(videoPath);
     }
 
     if (linkData.redirect) {
---
+++ src/decorators/link_decorators/pCloud.js
@@ -0,0 +1,5 @@
+function pCloud(link) {
+  return { link: link, redirect: true };
+}
+
+module.exports = pCloud;
